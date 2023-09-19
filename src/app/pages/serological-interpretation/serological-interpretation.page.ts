944919dea19919b32de872e163d2714c3a635610
Corentin <corentin.martinot@gmail.com>
2023-09-19 23:33:13 +0200

    on click, open img in navigator

--- src/app/pages/serological-interpretation/serological-interpretation.page.ts
+++ src/app/pages/serological-interpretation/serological-interpretation.page.ts
@@ -31,4 +31,8 @@ export class SerologicalInterpretationPage implements OnInit {
   goToHome() {
     this.navCtrl.navigateForward('home?extent-vhb-chronicle=${true}');
   }
+
+  viewImg(src: string) {
+    window.open(`https://vhb.netlify.app${src}`, '_blank');
+  }
 }
